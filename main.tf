90bd6f14df45b6c31aefe54f3de35cd3171986cd
onikan27 <ude0721juji@icloud.com>
2021-07-25 00:28:36 +0900

    feat: Add logic

--- main.tf
+++ main.tf
@@ -1,4 +1,21 @@
-provider "aws" {
-  region = "ap-northeast-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.50.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = var.region
+  profile = var.profile
+}
+
+resource "aws_vpc" "ex_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "test"
+  }
+}
